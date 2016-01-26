53607973c81a4e0cd7fb4fa3f53d2142000ac4cd
brandensiegle <brandensiegle@hotmail.com>
2016-01-25 17:20:47 -0800

    Repo Setup

---
+++ node.go
@@ -0,0 +1,16 @@
+/*
+Usage: go run node.go [ip:port] [id]
+	•[ip:port] : address of the key-value service
+	•[id] : a unique string identifier for the node
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+
+
+}
